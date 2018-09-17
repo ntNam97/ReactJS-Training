76dbb7b649d34f41c878d579ad197405a7e02470
ntNam97 <ntnamcnttk15@gmail.com>
2018-09-17 15:37:47 +0700

    finish

--- src/index.js
+++ src/index.js
@@ -1,56 +1,76 @@
 
+var todos;
+window.addEventListener("load", () => {
+    todos = new Todos();
+    todos.addEventListener();
+});
+
+
 class Todos{
   constructor(){
     this. todoList=JSON.parse(localStorage.getItem('TODOS'));
     if(!this.todoList){
       this.todoList=new Array();
     }
+    this.filter="ALL";
     this.render_saveList()
   }
 
   addEventListener()
   {
 
-    //press Enters
+    //press Enters or loses focus
     let input=document.getElementById("input");
+    input.setAttribute("onblur", "todos.addTodo();");
     input.addEventListener("keyup", (event) =>{
       if (event.keyCode === 13) {
-        this.addTodo(input);
+        this.addTodo();
       }
     });
 
     //filter All
+    
     let all=document.getElementById("all");
     all.onclick=()=>{
+      this.filter="ALL";
       this.render_saveList();
     }
 
     //filter Active
+    
     let active=document.getElementById("active");
     active.onclick=()=>{
-      this.filterActive();
+      this.filter="ACTIVE";
+      this.render_saveList();
     }
 
     //filter Completed
+    
     let completed=document.getElementById("completed");
     completed.onclick=()=>{
-      this.filterCompleted();
+      this.filter="COMPLETED";
+      this.render_saveList();
     }
   }
 
-  addTodo(input){
-    let todo={
-      task:input.value,
-      isCompleted:false,
-    };
-    this.todoList.push(todo);
-    input.value='';
-    this.render_saveList();
+  addTodo(){
+    let input=document.getElementById("input");
+    if(input.value!='')
+    {
+      let todo={
+        task:input.value,
+        isCompleted:false,
+      };
+      input.value='';
+      this.todoList.push(todo);
+
+      this.render_saveList();
+    }
   }
 
   removeTodo(index){
-      this.todoList.splice(index, 1);
-      this.render_saveList();
+    this.todoList.splice(index, 1);
+    this.render_saveList();
   }
 
   changeCompleted(index){
@@ -58,8 +78,36 @@ class Todos{
     this.render_saveList();
   }
 
-  filterActive(){
-    console.log('active');
+  changeTodo(index){
+    let todo=document.getElementById("todo"+index);
+
+    //clear child
+    todo.innerHTML='';
+
+    let input=document.createElement("input");
+    todo.appendChild(input);
+    input.focus();
+    input.value=this.todoList[index].task;
+    input.className="reInput";
+    input.setAttribute("id", "change"+index);
+    input.setAttribute("onblur", "todos.CompletedChangeTodo("+index+");");
+    input.addEventListener("keyup", (event) =>{
+      if (event.keyCode === 13) {
+        this.CompletedChangeTodo(index);
+      }
+    });
+  }
+
+  CompletedChangeTodo(index){
+    let input=document.getElementById("change"+index);
+    this.todoList[index].task=input.value;
+    this.render_saveList();
+  }
+
+  render_saveList(){
+    
+    //save list
+    localStorage.setItem("TODOS", JSON.stringify(this.todoList));
 
     //clear old list
     let list=document.getElementById("todos");
@@ -68,11 +116,37 @@ class Todos{
     }
 
     //render list
+    switch (this.filter) {
+      case "ALL":
+        this.filterAll();
+        break;
+      case "ACTIVE":
+        this.filterActive();
+        break;
+      case "COMPLETED":
+        this.filterCompleted();
+        break;
+    }
+  }
+
+  filterActive(){
+    let list=document.getElementById("todos");
+    let count=0;
     this.todoList.forEach( (element, index)=> {
 
       if(!element.isCompleted){
+        count++;
+
         let li=document.createElement("li");
-        li.setAttribute("class", "list-group-item")
+        li.setAttribute("class", "list-group-item");
+        li.setAttribute("id", "todo"+index);
+        li.setAttribute("ondblclick", "todos.changeTodo("+index+");");
+        li.onmouseover=()=>{
+          button. style.visibility = 'visible';
+        }
+        li.onmouseout=()=>{
+          button. style.visibility = 'hidden';
+        }
 
         //Checkbox completed
         let checkBox=document.createElement("input");
@@ -89,27 +163,34 @@ class Todos{
         let button=document.createElement("button");
         button.innerHTML='x';
         button.setAttribute("onclick", "todos.removeTodo("+index+");");
+        button. style.visibility = 'hidden';
         li.appendChild(button);
    
         list.appendChild(li);
       }
     });
+
+    let label=document.getElementById("count");
+    label.innerHTML=count+" items left";
   }
 
   filterCompleted(){
-     console.log('completed');
-
-    //clear old list
     let list=document.getElementById("todos");
-    while( list.firstChild ){
-      list.removeChild( list.firstChild );
-    }
-
-    //render list
+    let count=this.todoList.length;
     this.todoList.forEach( (element, index)=> {
       if(element.isCompleted){
+        count--;
+
         let li=document.createElement("li");
-        li.setAttribute("class", "list-group-item")
+        li.setAttribute("class", "list-group-item");
+        li.setAttribute("id", "todo"+index);
+        li.setAttribute("ondblclick", "todos.changeTodo("+index+");");
+        li.onmouseover=()=>{
+          button. style.visibility = 'visible';
+        }
+        li.onmouseout=()=>{
+          button. style.visibility = 'hidden';
+        }
 
         //Checkbox completed
         let checkBox=document.createElement("input");
@@ -129,35 +210,38 @@ class Todos{
         let button=document.createElement("button");
         button.innerHTML='x';
         button.setAttribute("onclick", "todos.removeTodo("+index+");");
+        button. style.visibility = 'hidden';
         li.appendChild(button);
      
         list.appendChild(li);
       }
-
     });
+
+    let label=document.getElementById("count");
+    label.innerHTML=count+" items left";
   }
 
-  render_saveList(){
-    console.log(this.todoList)
-    //save list
-    localStorage.setItem("TODOS", JSON.stringify(this.todoList));
-
-    //clear old list
+  filterAll(){
     let list=document.getElementById("todos");
-    while( list.firstChild ){
-      list.removeChild( list.firstChild );
-    }
-
-    //render list
+    let count=this.todoList.length;
     this.todoList.forEach( (element, index)=> {
       let li=document.createElement("li");
       li.setAttribute("class", "list-group-item")
+      li.setAttribute("id", "todo"+index);
+      li.setAttribute("ondblclick", "todos.changeTodo("+index+");");
+      li.onmouseover=()=>{
+          button. style.visibility = 'visible';
+        }
+        li.onmouseout=()=>{
+          button. style.visibility = 'hidden';
+        }
 
       //Checkbox completed
       let checkBox=document.createElement("input");
       checkBox.setAttribute("type", "checkBox");
       if(element.isCompleted)
       {
+        count--;
         checkBox.setAttribute("checked","true");
       }
       checkBox.setAttribute("onchange", "todos.changeCompleted("+index+");");
@@ -176,16 +260,16 @@ class Todos{
       let button=document.createElement("button");
       button.innerHTML='x';
       button.setAttribute("onclick", "todos.removeTodo("+index+");");
+      button. style.visibility = 'hidden';  
       li.appendChild(button);
    
       list.appendChild(li);
     });
+
+    let label=document.getElementById("count");
+    label.innerHTML=count+" items left";
   }
 
 }
 
-var todos;
-window.addEventListener("load", () => {
-    todos = new Todos();
-    todos.addEventListener();
-});
+
